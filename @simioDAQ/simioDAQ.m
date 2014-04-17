024ab40662ffcb349a26afe3c00ec8e9843094c7
matthewrsilver <matthew.r.silver@gmail.com>
2014-04-17 16:05:28 -0400

    Taking advantage of directory structure for objects in simioDAQ.

--- @simioDAQ/simioDAQ.m
+++ @simioDAQ/simioDAQ.m
@@ -96,88 +96,11 @@ classdef simioDAQ < dynamicprops
             %delete(self.dio);
         end
         
-        % Add a digital line
-        function addLine(self, line, port, direction, name)
-            
-            % Add the line to the dio object
-            addline(self.dio, line, port, direction, name);
- 
-            % Add a property used to get the value of this line
-            self.addprop(name);
-            
-            % Handle getting/setting
-            if strcmpi(direction, 'in')
-                %P.GetMethod = @(x)getvalue(self.dioudd, self.dio.(name).index);
-                self.(name) = @(x)getvalue(self.dioudd, self.dio.(name).index)== true;
-                self.env.io.addInterface(name, direction, self.(name));
-                
-            elseif strcmpi(direction, 'out')
-                %P.SetMethod = @(self,x)putvalue(self.dioudd, x, self.dio.(name).index);
-                self.(name) = @(x)putvalue(self.dioudd, x, self.dio.(name).index);
-                self.env.io.addInterface(name, direction, self.(name));
-            end
-            
-        end
-        
         % List the names of lines that have been added
         function lineNames = getLines(self)
             lineNames = self.dio.line.lineName;
         end
-        
-        % Add a strobe line
-        function addStrobeLine(self, line, port)
-            addline(self.dio, line, port, 'out', 'strobe');
-         
-            self.strobeIndex = self.dio.strobe.index;
-        end
-        
-        % Add code lines, specially handled for event codes
-        function addCodeLines(self, lines, ports)
-            
-            % Make sure the number of lines and ports is the same
-            assert(length(lines) == length(ports))
-            
-            % Iterate through the lines and add each
-            for cur = 1:length(lines)
-                name = ['codeLine' num2str(cur)];
-                addline(self.dio, lines(cur), ports(cur), 'out', name);
-                self.codeIndices = [self.codeIndices self.dio.(name).index];
-            end
-        end
-        
-        % Set event lines and trigger strobe
-        function strobeTime = strobeCode(self, decimalCode)
-                   
-            % If there's no strobe line, or no code lines, just quit
-            if isempty(self.strobeIndex) || isempty(self.codeIndices)
-                strobeTime = GetSecs;
-                return;
-            end
-            
-            % Set the values of the code lines
-            numBits = length(self.codeIndices);
-            code = fliplr(dec2binvec(decimalCode,numBits));
-            
-            % Check the length and truncate if needed, but WARN!
-            if length(code) > numBits
-                code = code(end-numBits+1:end);
-                disp('INSUFFICIENT CODE BITS, INFORMATION LOST');
-            end
-            
-            % Put the binary code on the lines
-            putvalue(self.dio.Line(self.codeIndices), code)
-            
-            % Set and reset the value of the strobe line
-            putvalue(self.dioudd, true, self.strobeIndex);
-            strobeTime = GetSecs;
-            WaitSecs(.0005);
-            putvalue(self.dioudd, false, self.strobeIndex);
-            
-            % Set the lines back to 0
-            putvalue(self.dio.Line(self.codeIndices), false(1, numBits));
-            
-        end
-            
+                                
     end
     
 end
