3bfe12a444c8704ea875a707397549fb6fc534d9
matthewrsilver <matthew.r.silver@gmail.com>
2014-04-24 12:47:10 -0400

    Configuration defaults loaded by base Simio class from m-files in config directory

--- Simio.m
+++ Simio.m
@@ -4,22 +4,68 @@ classdef Simio < SimioEnv
 %   as lever presses and eye movements though a DAQ and through
 %   ethernet, to display stimuli on the screen using PsychToolbox, and
 %   to mark important times with event codes through the DAQ.
-    
-    properties
-        
-        
-    end
-
+   
     methods
         
         % Constructor for the simio class
         function self = Simio(varargin)
            
+            % Add the config directory to the path 
+            addpath([fileparts(mfilename('fullname')) filesep 'config'])
+   
+            % Extract user configuration information, and merge
+            % with default information in config dir
+            for arg = 1:2:numel(varargin)
+                switch varargin{arg}
+                    case 'config'
+                        tmpConf = Simio.userConfig(varargin{arg+1});
+                    case 'codes'
+                        tmpCode = Simio.userCodes(varargin{arg+1});
+                    otherwise
+                        disp([varargin{arg} ' is not a valid parameter']);
+                end
+            end
+
             % Inherit SimioEnv
-            self@SimioEnv(varargin{:});
+            self@SimioEnv('config', tmpConf, 'codes', tmpCodes);
             
         end
               
+    end
+    
+    methods(Static)
+        
+        % Add user-supplied config information, overriding defaults
+        function mergeConfig = userConfig(newConfig)
+            
+            % Load the default config
+            mergeConfig = default_config;
+                
+            % Get the field names from the user supplied config
+            names = fieldnames(newConfig);
+            
+            % Iterate through the field names, and add to config
+            for field = 1:numel(names)
+                mergeConfig.(names{field}) = newConfig.(names{field});
+            end
+            
+        end
+        
+        % Add user-supplied config information, overriding defaults
+        function mergeCodes = userCodes(newCodes)
+            
+            % Load the default codes
+            mergeCodes = default_codes;
+                
+            % Get the field names from the user supplied config
+            names = fieldnames(newCodes);
+            
+            % Iterate through the field names, and add to config
+            for field = 1:numel(names)
+                mergeCodes.(names{field}) = newCodes.(names{field});
+            end
+            
+        end
         
     end
 end
