677410159030e788e2d2175af677fdc9836d5aaa
matthewrsilver <matthew.r.silver@gmail.com>
2014-05-12 16:25:37 -0400

    Improved wait function should be faster and handle error logic more cleanly

--- @SimioEnv/wait.m
+++ @SimioEnv/wait.m
@@ -1,98 +1,64 @@
-% Wait for conditions to be met
 function [err latency] = wait(self, duration, varargin)
+% Wait for conditions to be met
 
     % Handle timing
     startTime = GetSecs;
     endTime   = startTime + duration/1000;
+    
+    % Default outputs
     latency   = NaN;
+    numTests  = ceil((nargin-2)/3); 
+    err       = zeros(1, max(numTests, 1));
     
-    % Holders for argument types
-    argTypes  = {};
-    whileArgs = {};
-    untilArgs = {};
-    
-    % Store the property names and test values associated with
-    % while and associated with until
-    for arg = 1:3:numel(varargin)
-        if strcmp('while', varargin{arg})
-            whileArgs = {whileArgs{:} {varargin{arg+1} varargin{arg+2}}};
-            argTypes  = {argTypes{:} 'while'};
-        else
-            untilArgs = {untilArgs{:} {varargin{arg+1} varargin{arg+2}}};
-            argTypes  = {argTypes{:} 'until'};
-        end
+    % Handle the case where we're just waiting, and otherwise set
+    % the default output, using the number of tests specified
+    if numTests < 1
+        WaitSecs(endTime-GetSecs)
+        return;
     end
+       
+    % Determine which tests use 'until' logic
+    untils = strcmp(varargin(1:3:nargin-2), 'until');
     
-    % Use the conditions to set default output values
-    err       = ~isempty(untilArgs);
-    
-    % Now run the loop
+    % Run the loop
     while GetSecs < endTime
         
-        % First check 'while' arguments
-        failures = ~cellfun(@(x)self.io.(x{1})==x{2}, whileArgs);
-        if any(failures)
-            latency = (GetSecs - startTime)*1000;
-            err     = failures;
-            return;
-        end
+        % Iterate through tests on each loop run
+        for test = 1:numTests
         
-        % Now check 'until' arguments
-        if ~isempty(untilArgs)
-            compliant = cellfun(@(x)self.io.(x{1})==x{2}, untilArgs);
-            if all(compliant)
-                latency = (GetSecs - startTime)*1000;
-                err     = false;
+            % Base argument for current test
+            ifaceArg = (test-1)*3+2;
+            
+            % Check the current test
+            same = self.io.(varargin{ifaceArg}) == varargin{ifaceArg+1};
+            
+            % If 'until' and same, then this is a success
+            if untils(test) && same
+                latency = (GetSecs - startTime)*1000; 
                 return;
-            else
-                err = ~compliant;
+            end
+            
+            % If neither 'until' nor same, then this is an error
+            if ~(untils(test) || same)
+                latency = (GetSecs - startTime)*1000;
+                err(test) = 1;
             end
         end
-        
+    
         % Pause briefly to keep loop running in a reasonable way
         WaitSecs(0.0001);
+        
     end
     
+    % If we've run through the loop, then there were never any
+    % events to trigger an early exit. For 'while' tests, this is
+    % desirable and not an error, but for 'until' tests this is an
+    % error. Thus, all 'until' tests should be marked as errors.
+    err = untils;
+      
 end
 
 
-% Another implementation, incompatible with multiple tests, but
-% that may contain certain improvements...
 
 
-%         function [err latency] = wait(self, duration, condition, interface, requirement)
-%         
-%             % Handle timing
-%             startTime = GetSecs;
-%             endTime   = startTime + duration/1000;
-%             
-%             % Default outputs
-%             latency   = NaN;
-%             err       = 0;
-%             
-%             % Handle the case where we're just waiting
-%             if nargin < 3
-%                 WaitSecs(endTime-GetSecs)
-%                 return;
-%             end         
-%             
-%             % If this is an 'until' call, reverse the logic
-%             if strcmp('until', condition)
-%                err         = 1;
-%                requirement = ~requirement; 
-%             end
-%             
-%             % Run the loop
-%             while GetSecs < endTime
-%                 if self.io.(interface) ~= requirement
-%                     latency = (GetSecs - startTime)*1000;
-%                     err     = ~err;
-%                     return;
-%                 end                 
-%             end
-%             
-%             % Pause briefly to keep loop running in a reasonable way
-%             WaitSecs(0.0001);
-%             
-%         end
 
