04f224e4521381b1b018a45168793f300780df2f
matthewrsilver <matthew.r.silver@gmail.com>
2014-07-22 16:40:51 -0400

    Added support for drawing a circle (i.e. octagon) to EyeLink in @SimioEyeLink/eyeLinkDraw.m. Unfortunately, it appears that drawing directly to EyeLink and using the eyeLinkAutoDraw feature simultaneously are difficult. Can this be integrated into autoDraw?

--- @SimioEyeLink/eyeLinkDraw.m
+++ @SimioEyeLink/eyeLinkDraw.m
@@ -26,6 +26,30 @@ function eyeLinkDraw(self, command, details)
             % Draw a rectangle
             Eyelink('Command', sprintf('draw_box %d %d %d %d 15', details));
             
+        case {'circ', 'circle'}
+            % Draw an octagon to serve as a circle
+            n = 8;
+            
+            % Pull info from details:
+            r = details(1);
+            
+            if numel(details) >= 3
+                cX = details(2);
+                cY = details(3);
+            else
+                cX = self.displayCenter(1);
+                cY = self.displayCenter(2);
+            end
+            
+            % Start by generating coordinates for lines...
+            a = tan(pi/n)*r;
+            x = [r  a -a -r -r -a  a  r  r] + cX;
+            y = [a  r  r  a -a -r -r -a  a] + cY;
+            
+            for cmd = 1:n
+                Eyelink('Command', sprintf('draw_line %d %d %d %d 10', [x(cmd) y(cmd) x(cmd+1) y(cmd+1)]));
+            end
+            
         case {'cross', 'cue'}
             % Draw a cross
             Eyelink('Command', sprintf('draw_cross %d %d 10', details));
